58226bf7546b3f454e4bb574b5c56974e341b7a9
bleistivt <bleistivt@users.noreply.github.com>
2017-07-06 14:54:53 +0200

    wrong

--- class.conversationsusersearch.plugin.php
+++ class.conversationsusersearch.plugin.php
@@ -3,12 +3,12 @@
 $PluginInfo['conversationsusersearch'] = [
     'Name' => 'Conversations User Search',
     'Description' => 'Search for conversations by user',
-    'Version' => '1.0',
+    'Version' => '1.0.1',
     'MobileFriendly' => true,
     'Author' => 'Bleistivt',
     'AuthorUrl' => 'http://bleistivt.net',
     'License' => 'GNU GPL2',
-    'GitHub' => 'bleistivt/rssoptions'
+    'GitHub' => 'bleistivt/conversationsusersearch'
 ];
 
 class ConversationsUserSearchPlugin extends Gdn_Plugin {
